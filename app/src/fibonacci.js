5caeaee30ddb088d62b84585dbf893b0bca90aee
lipenghui <lipenghui@didichuxing.com>
2018-11-16 15:08:47 +0800

    test

--- app/src/fibonacci.js
+++ app/src/fibonacci.js
@@ -1,12 +1,12 @@
 
-var memory = []
+// var memory = []
 
-function fabonacci(n) {
-  if (memory[n] !== undefined) {
-    return memory [n]
-  }
-  return memory[n] = (n === 0 || n === 1) ? n : fabonacci(n - 1) + fabonacci(n - 2)
-}
+// function fabonacci(n) {
+//   if (memory[n] !== undefined) {
+//     return memory [n]
+//   }
+//   return memory[n] = (n === 0 || n === 1) ? n : fabonacci(n - 1) + fabonacci(n - 2)
+// }
 
 // var start  = new Date();
 // var result = fabonacci(500);
@@ -14,6 +14,6 @@ function fabonacci(n) {
 
 // console.log('fabonacci(%d) = %d, use time %dms.',   50,  result, end.getTime() - start.getTime());
 
-for (let i = 0; i <= 50; i++) {
-  console.log(fabonacci(i))
-}
+// for (let i = 0; i <= 50; i++) {
+//   console.log(fabonacci(i))
+// }
--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -13,6 +13,7 @@
     },
     created () {
       this.num = this.fibonacci(10)
+      this.printFib(100)
     },
 
     methods: {
@@ -21,6 +22,18 @@
           return n
         }
         return this.fibonacci(n - 1) + this.fibonacci(n - 2)
+      },
+
+      printFib (n) {
+        let array = []
+        for (let i = 0; i < n; i++) {
+          if (i < 2) {
+            array[i] = 1
+          } else {
+            array[i] = array[i-1] + array[i-2]
+          }
+        }
+        console.log(array)
       }
     }
   }
